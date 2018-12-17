4dfd54c36a07941345a2fcfb8623751331b2c8bc
HaChi <levuhachi@gmail.com>
2018-12-17 19:51:16 +0700

    hachi

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+import mlab
+from models.user import User
+from models.lazydata import Lazy
+
+mlab.connect()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ABC"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        form = request.form
+        u = form["username"]
+        p = form["password"]
+        found_user = User.objects(username=u).first()
+        if found_user == None:
+            return "No such user!!"
+        else:
+            if found_user["password"] == p:
+                session["token"] = u
+                return "Welcome!!!"
+                return redirect(url_for("lazythinking"))
+            else:
+                return "Wrong password"
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("sign_up.html")
+    else:
+        form = request.form
+        u = form["username"]
+        p = form["password"]
+        exist_user = User.objects(username=u).first()
+        if exist_user != None:
+            return "User already exist !!!!"
+        else:
+            m = User(username=u, password=p)
+            m.save()
+            return "Sign up successful" 
+
+@app.route("/lazythinking", methods = ["GET","POST"])
+def lazythinking():
+    if request.method == "GET":
+        return render_template("save_data.html")
+    elif request.method == "POST":
+        form = request.form
+        title = form["title"]
+        option = []
+        for k,v in form.items():
+            if k != "title":
+                newoption = v
+                option.append(newoption)
+        new_option = Lazy(title = title, option = option)
+        new_option.save()
+        return "Your choices have been saved"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- mlab.py
+++ mlab.py
@@ -1,13 +1,16 @@
 import mongoengine
 
-# mongodb://<dbuser>:<dbpassword>@ds235169.mlab.com:35169/lazydata
+# mongodb://<dbuser>:<dbpassword>@ds125342.mlab.com:25342/lazythinking
 
-host = "ds235169.mlab.com"
-port = 35169
-db_name = "lazydata"
-user_name = "test"
-password = "test123"
+host = "ds125342.mlab.com"
+port = 25342
+db_name = "lazythinking"
+user_name = "lazy1"
+password = "lazy123"
 
+#account mlab:
+#name: lazythinking
+#pass:c4e23lazythinking
 
 def connect():
     mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
@@ -15,7 +18,6 @@ def connect():
 def list2json(l):
     import json
     return [json.loads(item.to_json()) for item in l]
-
 
 def item2json(item):
     import json
